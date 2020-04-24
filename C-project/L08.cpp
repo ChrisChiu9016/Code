6013b410fba6cf7c1c5493689168b115f9077bce
ChrisChiu9016 <you@example.com>
2020-04-24 17:41:19 +0800

    L08.cpp update

--- C-project/L08.cpp
+++ C-project/L08.cpp
@@ -2,18 +2,24 @@
 #include <cmath>
 
 using namespace std;
-
-const int NtoU=30,NtoE=40;
+const int UtoN=30,EtoN=40;
+double vntd,vusd,veud;
 
 class bankacc
 {
 public:
-    bankacc(const int ntd,const int usd,const int eud);
-    bankacc operator+(const bankacc&);
-    bankacc operator-(const bankacc&);
-    bankacc operator*(const bankacc&);
-    bankacc operator%(const bankacc&);
-    bankacc operator/(const bankacc&);
+    bankacc(const int ntd,const int usd,const int eud)
+    {
+        setNTD(ntd);
+        setUSD(usd);
+        setEUD(eud);
+    }
+    bankacc operator+(const bankacc& that);
+    bankacc operator-(const bankacc& that);
+    bankacc operator*(const bankacc& that);
+    bankacc operator%(const bankacc& that);
+    bankacc operator/(const bankacc& that);
+    int reset(const int ntd,const int usd,const int eud);
     int setNTD(const int ntd);
     int setUSD(const int usd);
     int setEUD(const int eud);
@@ -22,38 +28,43 @@ public:
     double valueINEUD();
     void output();
 private:
-    int ntd,usd,eud;
+    double ntd=0,usd=0,eud=0;
 };
 
-bankacc bankacc::operator+(const bankacc&that)
+bankacc bankacc::operator+(const bankacc& that)
 {
-    return(this->ntd+that.ntd,
-           this->usd+that.usd,
-           this->eud+that.eud);
+    this->ntd+=that.ntd;
+    this->usd+=that.usd;
+    this->eud+=that.eud;
+    return bankacc(this->ntd,this->usd,this->eud);
 }
-bankacc bankacc::operator-(const bankacc&that)
+bankacc bankacc::operator-(const bankacc& that)
 {
-    return(this->ntd-that.ntd,
-           this->usd-that.usd,
-           this->eud-that.eud);
+    this->ntd-=that.ntd;
+    this->usd-=that.usd;
+    this->eud-=that.eud;
+    return bankacc(this->ntd,this->usd,this->eud);
 }
-bankacc bankacc::operator%(const bankacc&that)
+bankacc bankacc::operator%(const bankacc& that)
 {
-    return(abs(this->ntd-that.ntd),
-           abs(this->usd-that.usd),
-           abs(this->eud-that.eud));
+    this->ntd=abs(this->ntd-that.ntd);
+    this->usd=abs(this->usd-that.usd);
+    this->eud=abs(this->eud-that.eud);
+    return bankacc(this->ntd,this->usd,this->eud);
 }
-bankacc bankacc::operator*(const bankacc&that)
+bankacc bankacc::operator*(const bankacc& that)
 {
-    return(this->ntd+0.99*that.ntd,
-           this->usd+0.99*that.usd,
-           this->eud+0.99*that.eud);
+    this->ntd+=0.99*that.ntd;
+    this->usd+=0.99*that.usd;
+    this->eud+=0.99*that.eud;
+    return bankacc(this->ntd,this->usd,this->eud);
 }
-bankacc bankacc::operator/(const bankacc&that)
+bankacc bankacc::operator/(const bankacc& that)
 {
-    return(this->ntd-1.01*that.ntd,
-           this->usd-1.01*that.usd,
-           this->eud-1.01*that.eud);
+    this->ntd-=1.01*that.ntd;
+    this->usd-=1.01*that.usd;
+    this->eud-=1.01*that.eud;
+    return bankacc(this->ntd,this->usd,this->eud);
 }
 int bankacc::setNTD(const int ntd)
 {
@@ -69,20 +80,68 @@ int bankacc::setEUD(const int eud)
 }
 void bankacc::output()
 {
+    cout << "NTD:";
     valueINNTD();
+    cout << " USD:";
     valueINUSD();
+    cout << " EUD:";
     valueINEUD();
+    cout << endl;
 }
 double bankacc::valueINNTD()
 {
-    int vntd;
-    vntd=this->ntd+this->usd*NtoU+this*NtoE;
+    vntd=this->ntd+this->usd*UtoN+this->eud*EtoN;
+    cout << vntd;
 }
 double bankacc::valueINUSD()
 {
-
+    vusd=vntd/30;
+    cout << vusd;
 }
 double bankacc::valueINEUD()
 {
 
+    veud=vntd/40;
+    cout << veud;
+}
+int bankacc::reset(const int ntd,const int usd,const int eud)
+{
+    setNTD(ntd);
+    setUSD(usd);
+    setEUD(eud);
+}
+
+int main()
+{
+    double x, y, z, a, b, c;
+    cin >> x >> y >> z >> a >> b >> c;
+    bankacc acct1(x,y,z),acct2(a,b,c);
+    acct1.output();
+    acct2.output();
+
+    acct1 + acct2;
+    cout << "+:";
+    acct1.output();
+
+    acct1.reset(x,y,z);
+    acct1 - acct2;
+    cout << "-:";
+    acct1.output();
+
+    acct1.reset(x,y,z);
+    acct1%acct2;
+    cout << "%:";
+    acct1.output();
+
+    acct1.reset(x,y,z);
+    acct1*acct2;
+    cout << "*:";
+    acct1.output();
+
+    acct1.reset(x,y,z);
+    acct1/acct2;
+    cout << "/:";
+    acct1.output();
+
+    return 0;
 }
